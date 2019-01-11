47c27a7b5282cedf6d324a3d20f33425ccc987a3
Erik Ulberg <ulberge@gmail.com>
2019-01-11 08:37:07 -0500

    Updates

---
+++ precache-manifest.0a79e5a48d87da332b28cb01dd9bb1fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "caf2146784860a0929bf",
+    "url": "/static/js/main.caf21467.chunk.js"
+  },
+  {
+    "revision": "d3d5f5ec9f00edb4ca1e",
+    "url": "/static/js/1.d3d5f5ec.chunk.js"
+  },
+  {
+    "revision": "caf2146784860a0929bf",
+    "url": "/static/css/main.03a43360.chunk.css"
+  },
+  {
+    "revision": "b5621ec4c5094a5447c41935206c9ba4",
+    "url": "/index.html"
+  }
+];
