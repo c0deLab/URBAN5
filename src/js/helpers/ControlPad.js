5e20d3e67bb5502f3d2c774a12ab0ea415568791
Erik Ulberg <ulberge@gmail.com>
2020-02-21 10:45:19 -0500

    add restart check

--- src/js/helpers/ControlPad.js
+++ src/js/helpers/ControlPad.js
@@ -26,7 +26,7 @@ export default class ControlPad {
     // need to retrieve every time for Chrome
     const gamepads = navigator.getGamepads();
     // assume there is only one gamepad hooked up
-    if (gamepads && gamepads.length === 1 && gamepads[0].buttons) {
+    if (gamepads && gamepads.length > 0 && gamepads[0] && gamepads[0].buttons) {
       gamepads[0].buttons.forEach((button, i) => {
         // if currently pressed and cache doesn't show that it was being pressed, fire event
         if (button.pressed && !this.buttonCache[i]) {
