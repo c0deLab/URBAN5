564e3b57476e11aae1ad146afc3fe10091a79be9
Erik Ulberg <ulberge@gmail.com>
2020-02-21 09:27:23 -0500

    Updates

---
+++ precache-manifest.60359f32b5f2c2d310e8bbb7163ba544.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95ae6a75de8e261d6c3d",
+    "url": "/URBAN5/static/js/runtime~main.95ae6a75.js"
+  },
+  {
+    "revision": "7ac765dcff8dcdb6010c",
+    "url": "/URBAN5/static/js/main.7ac765dc.chunk.js"
+  },
+  {
+    "revision": "33648706cc5c99544b7d",
+    "url": "/URBAN5/static/js/1.33648706.chunk.js"
+  },
+  {
+    "revision": "7ac765dcff8dcdb6010c",
+    "url": "/URBAN5/static/css/main.d4d89f07.chunk.css"
+  },
+  {
+    "revision": "9a4c9a242fb7de9a58d452319103ee11",
+    "url": "/URBAN5/index.html"
+  }
+];
